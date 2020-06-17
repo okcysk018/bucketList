0bc27c1bf939df739e5b5f2153c4be972e1eb346
okcysk018 <a13.gh38@gmail.com>
2020-06-17 14:48:09 +0900

    repair config Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.14.1"
 # Capistranoのログの表示に利用する
 set :application, "bucketList"
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, "git@github.com:okcysk018/bucket.git"
+set :repo_url, "git@github.com:okcysk018/bucketList.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
