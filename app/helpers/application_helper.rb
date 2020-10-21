e1cc632f21a7c0244e8f1c555572ada86bd1c5fb
okcysk018 <a13.gh38@gmail.com>
2020-10-21 16:57:48 +0900

    refactoring & best_pratice etc
    ■実装内容
    ・不要なヘルパーの削除
    ・render best_practice
    ・部分テンプレート追加・修正
    ・ヘルパーメソッド追加
    ・lメソッドでの日付表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def current_user_has?(instance)
+    user_signed_in? && current_user == instance.user
+  end
+
+  # def not_current_user_is?(instance)
+  #   current_user != instance.user
+  # end
+
 end
