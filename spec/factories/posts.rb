a0ca837cbede8229a974c4121bdfdc3492dacaf7
okcysk018 <a13.gh38@gmail.com>
2020-10-30 22:04:13 +0900

    Rspec for post model
    ■実装内容
    ・traitによる属性オブジェクト化
    ・継承クラスの作成
    ・アウトラインの変更

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,51 +2,90 @@ FactoryBot.define do
 
   factory :post, class: Post do
     title       { Faker::Nation.nationality }
-    deadline    {'2020-01-01'}
-    budget      {1000}
+    deadline    { '2020-01-01' }
+    budget      { 1000 }
     user
-  end
 
-  factory :post_eleven_images, class: Post do
-    title       { Faker::Nation.nationality }
-    deadline    {'2020-01-01'}
-    budget      {1000}
-    user
-    after(:build) do |post|
-      # carrierwaveの場合
-      post.images << build_list(:image, 11)
+    trait :title_length_max do
+      title       { Faker::Base.regexify("[aあ]{40}") }
+    end
+
+    trait :title_length_over do
+      title       { Faker::Lorem.characters(number: 41) }
+    end
+
+    trait :budget_length_max do
+      budget      { 9999999 }
+    end
+
+    trait :budget_length_over do
+      budget      { 10000000 }
+    end
+
+    trait :budget_zero do
+      budget      { 0 }
+    end
+
+    trait :budget_minus do
+      budget      { -1 }
+    end
+
+    trait :budget_decimal do
+      budget      { Faker::Number.decimal(l_digits: 2) }
+    end
+
+    trait :with_address do
+      address     { '日本、東京都東京' }
+    end
+
+    trait :with_placeId do
+      place_id    { 'ChIJXSModoWLGGARILWiCfeu2M0' }
+    end
+
+    trait :with_reputation do
+      reputation  {Faker::Number.between(from: 1, to: 5)}
+    end
+
+    trait :with_priority do
+      priority    {Faker::Number.within(range: 1..5)}
+    end
+
+    trait :done_post do
+      done_flag    { 1 }
+    end
+
+    trait :private_post do
+      private_flag  { 1 }
+    end
+
+    trait :with_description do
+      description  { Faker::Lorem.paragraph(sentence_count: 5) }
+    end
+
+    trait :post_ten_images do
+      after(:build) do |post|
+        # carrierwaveの場合
+        post.images << build_list(:image, 10)
+      end
+    end
+
+    trait :post_ten_tasks do
+      after(:build) do |post|
+        post.tasks << build_list(:task, 10)
+      end
+    end
+
+    factory :post_eleven_images, class: Post do
+      after(:build) do |post|
+        # carrierwaveの場合
+        post.images << build_list(:image, 11)
+      end
+    end
+
+    factory :post_eleven_tasks, class: Post do
+      after(:build) do |post|
+        post.tasks << build_list(:task, 11)
+      end
     end
   end
-
-  factory :post_ten_images, class: Post do
-    title       { Faker::Nation.nationality }
-    deadline    {'2020-01-01'}
-    budget      {1000}
-    user
-    after(:build) do |post|
-      # carrierwaveの場合
-      post.images << build_list(:image, 10)
-    end
-  end
-
-  factory :post_eleven_tasks, class: Post do
-    title       { Faker::Nation.nationality }
-    deadline    {'2020-01-01'}
-    budget      {1000}
-    user
-    after(:build) do |post|
-      post.tasks << build_list(:task, 11)
-    end
-  end
-
-  factory :post_ten_tasks, class: Post do
-    title       { Faker::Nation.nationality }
-    deadline    {'2020-01-01'}
-    budget      {1000}
-    user
-    after(:build) do |post|
-      post.tasks << build_list(:task, 10)
-    end
-  end
-
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,141 +2,139 @@
 require 'rails_helper'
 describe Post do
   describe '#create' do
-    it "正常系チェック" do
-      post = build(:post)
-      expect(post).to be_valid
+    context '正常系' do
+      it "最小限入力項目登録チェック" do
+        expect(build(:post)).to be_valid
+      end
+
+      it "全入力項目登録チェック" do
+        expect(build(:post, :with_address, :with_placeId, :with_reputation, :with_priority, :with_description, :post_ten_images, :post_ten_tasks)).to be_valid
+      end
+
+      it "title文字数チェック 40文字以下で登録できる" do
+        expect(build(:post, :title_length_max)).to be_valid
+      end
+
+      it "budget文字数チェック 7桁以下で登録できる" do
+        expect(build(:post, :budget_length_max)).to be_valid
+      end
+
+      it "budget文字数チェック 0で登録できる" do
+        expect(build(:post, :budget_zero)).to be_valid
+      end
+
+      it "地図情報制限チェック address, place_id入力" do
+        expect(build(:post, :with_address, :with_placeId)).to be_valid
+      end
+
+      it "reputation制限チェック 選択候補の場合" do
+        expect(build(:post, :with_reputation)).to be_valid
+      end
+
+      it "priority制限チェック 選択候補の場合" do
+        expect(build(:post, :with_priority)).to be_valid
+      end
+
+      it "image枚数制限チェック 10枚以下の場合" do
+        expect(build(:post, :post_ten_images)).to be_valid
+      end
+
+      it "task個数制限チェック 10以下の場合" do
+        expect(build(:post, :post_ten_tasks)).to be_valid
+      end
+
     end
 
-    it "title必須チェック" do
-      post = build(:post, title: nil)
-      post.valid?
-      expect(post.errors[:title]).to include("を入力してください")
+    context '異常系' do
+      it "title必須チェック登録不可" do
+        post = build(:post, title: nil)
+        post.valid?
+        expect(post.errors[:title]).to include("を入力してください")
+      end
+
+      it "budget必須チェック登録不可" do
+        post = build(:post, budget: nil)
+        post.valid?
+        expect(post.errors[:budget]).to include("を入力してください")
+      end
+
+      it "deadline必須チェック登録不可" do
+        post = build(:post, deadline: nil)
+        post.valid?
+        expect(post.errors[:deadline]).to include("を入力してください")
+      end
+
+      it "user_id必須チェック登録不可" do
+        post = build(:post, user_id: nil)
+        post.valid?
+        expect(post.errors[:user]).to include("を入力してください")
+      end
+
+      it "title文字数チェック 41文字以上で登録できない" do
+        post = build(:post, :title_length_over)
+        post.valid?
+        expect(post.errors[:title]).to include("は40文字以内で入力してください")
+      end
+
+      it "budget文字数チェック 8桁以上で登録できない" do
+        post = build(:post, :budget_length_over)
+        post.valid?
+        expect(post.errors[:budget]).to include("は10000000より小さい値にしてください")
+      end
+
+      it "budget文字数チェック マイナスで登録できない" do
+        post = build(:post, :budget_minus)
+        post.valid?
+        expect(post.errors[:budget]).to include("は0以上の値にしてください")
+      end
+
+      it "budget文字数チェック 少数で登録できない" do
+        post = build(:post, :budget_decimal)
+        post.valid?
+        expect(post.errors[:budget]).to include("は整数で入力してください")
+      end
+
+      # it "deadline日付制限チェック 存在しない日付で登録できない" do
+      #   post = build(:post, deadline: '2020-02-31')
+      #   post.valid?
+      #   expect(post.errors[:deadline]).to include("は不正な値です")
+      # end
+
+      it "地図情報制限チェック place_id未入力の場合登録不可" do
+        post = build(:post, :with_address)
+        post.valid?
+        expect(post.errors[:address]).to include("は候補の中から選択してください")
+      end
+
+      it "地図情報制限チェック address未入力の場合登録不可" do
+        post = build(:post, :with_placeId)
+        post.valid?
+        expect(post.errors[:address]).to include("を入力してください")
+      end
+
+      it "reputation制限チェック 選択候補以外の場合登録不可" do
+        post = build(:post, reputation: 6)
+        post.valid?
+        expect(post.errors[:reputation]).to include("は一覧にありません")
+      end
+
+      it "priority制限チェック 選択候補以外の場合登録不可" do
+        post = build(:post, priority: 0)
+        post.valid?
+        expect(post.errors[:priority]).to include("は一覧にありません")
+      end
+
+      it "image枚数制限チェック 11枚以上の場合登録不可" do
+        post = build(:post_eleven_images)
+        post.valid?
+        expect(post.errors[:images]).to include("は10文字以内で入力してください")
+      end
+
+      it "task個数制限チェック 11以上の場合登録不可" do
+        post = build(:post_eleven_tasks)
+        post.valid?
+        expect(post.errors[:tasks]).to include("は10文字以内で入力してください")
+      end
     end
-
-    it "budget必須チェック" do
-      post = build(:post, budget: nil)
-      post.valid?
-      expect(post.errors[:budget]).to include("を入力してください")
-    end
-
-    it "deadline必須チェック" do
-      post = build(:post, deadline: nil)
-      post.valid?
-      expect(post.errors[:deadline]).to include("を入力してください")
-    end
-
-    it "title文字数チェック 41文字以上で登録できない" do
-      title41 = Faker::Lorem.characters(number: 41)
-      post = build(:post, title: title41)
-      post.valid?
-      expect(post.errors[:title]).to include("は40文字以内で入力してください")
-    end
-
-    it "title文字数チェック 40文字以下で登録できる" do
-      title40 = Faker::Base.regexify("[aあ]{40}")
-      post = build(:post, title: title40)
-      expect(post).to be_valid
-    end
-
-    it "budget文字数チェック 8桁以上で登録できない" do
-      number8 = 10000000
-      post = build(:post, budget: number8)
-      post.valid?
-      expect(post.errors[:budget]).to include("は10000000より小さい値にしてください")
-    end
-
-    it "budget文字数チェック 7桁以下で登録できる" do
-      number7 = 9999999
-      post = build(:post, budget: number7)
-      expect(post).to be_valid
-    end
-
-    it "budget文字数チェック 0で登録できる" do
-      number0 = 0
-      post = build(:post, budget: number0)
-      expect(post).to be_valid
-    end
-
-    it "budget文字数チェック マイナスで登録できない" do
-      minusNumber = -1
-      post = build(:post, budget: minusNumber)
-      post.valid?
-      expect(post.errors[:budget]).to include("は0以上の値にしてください")
-    end
-
-    it "budget文字数チェック 少数で登録できない" do
-      decimalNumber = Faker::Number.decimal(l_digits: 2)
-      post = build(:post, budget: decimalNumber)
-      post.valid?
-      expect(post.errors[:budget]).to include("は整数で入力してください")
-    end
-
-    # it "deadline日付制限チェック 存在しない日付で登録できない" do
-    #   post = build(:post, deadline: '2020-02-31')
-    #   post.valid?
-    #   expect(post.errors[:deadline]).to include("は不正な値です")
-    # end
-
-    it "地図情報制限チェック place_id未入力の場合" do
-      post = build(:post, address: '日本、東京都東京')
-      post.valid?
-      expect(post.errors[:address]).to include("は候補の中から選択してください")
-    end
-
-    it "地図情報制限チェック address未入力の場合" do
-      post = build(:post, place_id: 'ChIJXSModoWLGGARILWiCfeu2M0')
-      post.valid?
-      expect(post.errors[:address]).to include("を入力してください")
-    end
-
-    it "地図情報制限チェック 登録可能" do
-      post = build(:post, address: '日本、東京都東京', place_id: 'ChIJXSModoWLGGARILWiCfeu2M0')
-      expect(post).to be_valid
-    end
-
-    it "reputation制限チェック 選択候補以外の場合" do
-      post = build(:post, reputation: 6)
-      post.valid?
-      expect(post.errors[:reputation]).to include("は一覧にありません")
-    end
-
-    it "reputation制限チェック 選択候補の場合" do
-      post = build(:post, reputation: Faker::Number.between(from: 1, to: 5))
-      expect(post).to be_valid
-    end
-
-    it "priority制限チェック 選択候補以外の場合" do
-      post = build(:post, priority: 0)
-      post.valid?
-      expect(post.errors[:priority]).to include("は一覧にありません")
-    end
-
-    it "priority制限チェック 選択候補の場合" do
-      post = build(:post, priority: Faker::Number.within(range: 1..5))
-      expect(post).to be_valid
-    end
-
-    it "image枚数制限チェック 11枚以上の場合" do
-      post = build(:post_eleven_images)
-      post.valid?
-      expect(post.errors[:images]).to include("は10文字以内で入力してください")
-    end
-
-    it "image枚数制限チェック 10枚以下の場合" do
-      post = build(:post_ten_images)
-      expect(post).to be_valid
-    end
-
-    it "task個数制限チェック 11以上の場合" do
-      post = build(:post_eleven_tasks)
-      post.valid?
-      expect(post.errors[:tasks]).to include("は10文字以内で入力してください")
-    end
-
-    it "task個数制限チェック 10以下の場合" do
-      post = build(:post_ten_tasks)
-      expect(post).to be_valid
-    end
-
   end
 end
